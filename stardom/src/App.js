dd2f1ec1f24ecb8f2cb9536e2b7c8b3a3123aea1
16121a1228 <gjyothsna1228@gmail.com>
2022-01-06 13:31:54 +0530

    commit

---
+++ stardom/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Home from './component/Home';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header>
+        <h1><b>prostars</b></h1>
+      </header>
+      <main>
+        <div>
+          <Home />
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
